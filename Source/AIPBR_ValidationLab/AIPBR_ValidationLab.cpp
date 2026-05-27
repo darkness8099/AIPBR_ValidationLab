3c151c44222f9208cd88955b5d24dfc87a00a663
岸波白野 <63482690+darkness8099@users.noreply.github.com>
2026-05-28 02:25:00 +0900

    Initial Unreal validation project

---
+++ Source/AIPBR_ValidationLab/AIPBR_ValidationLab.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "AIPBR_ValidationLab.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, AIPBR_ValidationLab, "AIPBR_ValidationLab" );
